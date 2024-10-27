540dac0f3f238346e2f90163dcfe948e098b3fb9
FeiYan <yan00126@algonquinlive.com>
2024-10-27 16:38:45 -0400

    delete commented lines

--- server/index.js
+++ server/index.js
@@ -11,9 +11,7 @@ app.use(cors()); // Allow all origins (you may want to configure this for produc
 
 // Connect to MongoDB
 // mongoose
-//     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => console.log('Connected to MongoDB'))
-//     .catch((err) => console.error('Failed to connect to MongoDB:', err));
+
 
 mongoose
     .connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
